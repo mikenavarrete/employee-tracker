c4e2e011190d78220cb7fd955369ef76dff56c2d
Michael Navarrete <mikenavarrete1998@gmail.com>
2024-04-17 16:09:33 -0400

    functioning cli

--- cli.js
+++ cli.js
@@ -1,74 +1,77 @@
 const inquirer = require( 'inquirer' );
 const mysql2 = require('mysql2');
 
-const connection = mysql.createConnection({
+const connection = mysql2.createConnection({
     host: 'localhost',
-    port: 3001,
+    port: 3306,
     user: 'root',
     password: 'ROOTroot',
     database: 'tracker'
 });
 
-inquirer
-    .prompt([
-       {
-        name: 'action',
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: [
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role',
-            'Exit'
-        ]
-       } 
-    ])
-    .then((answers) => {
-        switch (answers.action) {
-            case 'View all departments':
-                viewDepartments();
-                break;
+function startApp(){
+    inquirer
+        .prompt([
+            {
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ]
+        } 
+        ])
+        .then((answers) => {
+            switch (answers.action) {
+                case 'View all departments':
+                    viewDepartments();
+                    break;
 
-            case 'View all roles':
-                viewRoles();
-                break;
+                case 'View all roles':
+                    viewRoles();
+                    break;
 
-            case 'View all employees':
-                viewEmployees();
-                break;
+                case 'View all employees':
+                    viewEmployees();
+                    break;
 
-            case 'Add a department':
-                addDepartment();
-                break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
 
-            case 'Add a role':
-                addRole();
-                break;
+                case 'Add a role':
+                    addRole();
+                    break;
 
-            case 'Add an employee':
-                addEmployee();
-                break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
 
-            case 'Update an employee role':
-                updateEmployeeRole();
-                break;
+                case 'Update an employee role':
+                    updateEmployeeRole();
+                    break;
 
-            case 'Exit':
-                connection.end();
-                break;
-        }
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            console.error("Prompt couldn't be rendered in the current environment") 
-        } else {
-            console.error("Something else went wrong")
-          }
-    });
+                case 'Exit':
+                    connection.end();
+                    break;
+            }
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                console.error("Prompt couldn't be rendered in the current environment") 
+            } else {
+                console.error("Something else went wrong")
+            }
+        });
+    }
+
 
 
     function viewDepartments(){
@@ -141,3 +144,70 @@ inquirer
             });
         });
     }
+
+    function addEmployee() {
+        inquirer.prompt([
+            {
+                name: 'first_name',
+                type: 'input',
+                message: 'Enter the first name of the employee:'
+            },
+            {
+                name: 'last_name',
+                type: 'input',
+                message: 'Enter the last name of the employee:'
+            },
+            {
+                name: 'role_id',
+                type: 'input',
+                message: 'Enter the role ID for this employee:'
+            },
+            {
+                name: 'manager_id',
+                type: 'input',
+                message: 'Enter the manager ID for this employee (optional):'
+            }
+        ]).then((answers) => {
+            const query = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+            connection.query(query, [answers.first_name, answers.last_name, answers.role_id, answers.manager_id], (err, res) => {
+                if (err) throw err;
+                console.log('Employee added successfully!');
+                startApp();
+            });
+        });
+    }
+
+    function updateEmployeeRole() {
+        const query = 'SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee';
+        connection.query(query, (err, employees) => {
+            if (err) throw err;
+    
+            const employeeChoices = employees.map(employee => ({
+                name: employee.name,
+                value: employee.id
+            }));
+    
+            inquirer.prompt([
+                {
+                    name: 'employeeId',
+                    type: 'list',
+                    message: 'Select an employee to update:',
+                    choices: employeeChoices
+                },
+                {
+                    name: 'roleId',
+                    type: 'input',
+                    message: 'Enter the new role ID for this employee:'
+                }
+            ]).then((answers) => {
+                const query = 'UPDATE employee SET role_id = ? WHERE id = ?';
+                connection.query(query, [answers.roleId, answers.employeeId], (err, res) => {
+                    if (err) throw err;
+                    console.log('Employee role updated successfully!');
+                    startApp();
+                });
+            });
+        });
+    }
+
+    startApp();
